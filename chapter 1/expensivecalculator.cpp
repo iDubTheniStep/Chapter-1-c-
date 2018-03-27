668e7056c85cfa02b88a1154b6fbf9041372fb72
iDubTheniStep <dp201524@students.davenport.k12.ia.us>
2018-03-27 13:06:59 -0500

    update

---
+++ chapter 1/expensivecalculator.cpp
@@ -0,0 +1,18 @@
+/**
+some calculator
+3/27/18
+Author: John BRORO
+**/
+#include<iostream>
+int main()
+{
+	std::cout << "7 + 3 = " << 7.0 + 3.0 << std::endl;
+	std::cout << "7 - 3 = " << 7.0 - 3.0 << std::endl;
+	std::cout << "7 * 3 = " << 7.0 * 3.0 << std::endl;
+	std::cout << "7 / 3 = " << 7.0 / 3.0 << std::endl;
+	std::cout << "7 % 3 = " << 7 % 3 << std::endl;
+	std::cout << "7 / 3 x 5 = " << 7 + 3 *5 << std::endl;
+	std::cout << "(7 + 3) x 5 = " << (7+3)*5 << std::endl;
+	system("pause");
+		return 0;
+}
